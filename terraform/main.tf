ff2b693fe6f766e094948b9829885081bf04572b
vibck <viktor.beck71@web.de>
2025-03-06 11:25:54 +0100

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,36 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+# VPC erstellen
+resource "aws_vpc" "react_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
+
+# Subnetz erstellen
+resource "aws_subnet" "react_subnet" {
+  vpc_id = aws_vpc.react_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
+}
+
+# EC2-Instanz mit zugewiesenem Subnetz
 resource "aws_instance" "react_ec2" {
   ami           = "ami-07eef52105e8a2059" 
   instance_type = "t2.micro"
   key_name      = "universal"
+  subnet_id     = aws_subnet.react_subnet.id  # Subnetz zuweisen
+
+  security_groups = [aws_security_group.react_sg.name]  # Sicherheitsgruppe zuweisen
 
   tags = {
     Name = "React-EC2"
   }
 }
 
+# Sicherheitsgruppe erstellen
 resource "aws_security_group" "react_sg" {
   name        = "react-security-group-new"
   description = "Erlaubt HTTP und SSH"
@@ -38,6 +58,8 @@ resource "aws_security_group" "react_sg" {
   }
 }
 
+# Output: öffentliche IP der EC2-Instanz
 output "public_ip" {
   value = aws_instance.react_ec2.public_ip
 }
+
