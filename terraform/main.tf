ebad27108facce6de74aaf70928894c106f61308
vibck <viktor.beck71@web.de>
2025-03-06 12:20:33 +0100

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "react_ec2" {
 # Sicherheitsgruppe erstellen
 resource "aws_security_group" "react_sg" {
   name        = "react-security-group-new"
-  description = "Erlaubt HTTP und SSH"
+  description = "Erlaubt HTTP, HTTPS und SSH"
   vpc_id      = aws_vpc.react_vpc.id
 
   ingress {
@@ -51,12 +51,48 @@ resource "aws_security_group" "react_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# Internet Gateway erstellen
+resource "aws_internet_gateway" "react_igw" {
+  vpc_id = aws_vpc.react_vpc.id
+
+  tags = {
+    Name = "React-IGW"
+  }
+}
+
+# Route Table erstellen
+resource "aws_route_table" "react_rt" {
+  vpc_id = aws_vpc.react_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.react_igw.id
+  }
+
+  tags = {
+    Name = "React-RT"
+  }
+}
+
+# Route Table mit Subnet verbinden
+resource "aws_route_table_association" "react_rta" {
+  subnet_id      = aws_subnet.react_subnet.id
+  route_table_id = aws_route_table.react_rt.id
 }
 
 # Output: öffentliche IP der EC2-Instanz
