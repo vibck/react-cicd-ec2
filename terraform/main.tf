28de79393c680fef9cdd159f334a21cc722f3a72
vibck <viktor.beck71@web.de>
2025-03-06 11:28:29 +0100

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,20 +11,20 @@ resource "aws_vpc" "react_vpc" {
 
 # Subnetz erstellen
 resource "aws_subnet" "react_subnet" {
-  vpc_id = aws_vpc.react_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-central-1a"
+  vpc_id                  = aws_vpc.react_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
 }
 
-# EC2-Instanz mit zugewiesenem Subnetz
+# EC2-Instanz erstellen
 resource "aws_instance" "react_ec2" {
   ami           = "ami-07eef52105e8a2059" 
   instance_type = "t2.micro"
   key_name      = "universal"
+  
   subnet_id     = aws_subnet.react_subnet.id  # Subnetz zuweisen
-
-  security_groups = [aws_security_group.react_sg.name]  # Sicherheitsgruppe zuweisen
+  vpc_security_group_ids = [aws_security_group.react_sg.id]  # Sicherheitsgruppe zuweisen
 
   tags = {
     Name = "React-EC2"
@@ -62,4 +62,5 @@ resource "aws_security_group" "react_sg" {
 output "public_ip" {
   value = aws_instance.react_ec2.public_ip
 }
+
 
